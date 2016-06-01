8a0be31244373a26aa52b983d7e14305c1cc1523
qiuzhong <zhongx.qiu@intel.com>
2016-06-01 14:03:57 +0800

    Add a semicolon for better JS style.

--- node/qunit/tests/tests.js
+++ node/qunit/tests/tests.js
@@ -2,7 +2,7 @@ QUnit.module('tests');
 
 // A test case depend on your own projects
 test("that a is true", function () {
-  ok(myObj.a)
+  ok(myObj.a);
 });
 
 // A test case that does not depend on your own projects
